a65a035e3c2ecbb3d0f761da73955eaf7667357e
tk1351 <takakikitsui0111@gmail.com>
2024-07-01 21:04:14 +0900

    feat: read all

--- api/main.go
+++ api/main.go
@@ -1,13 +1,84 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Todo struct {
+	*gorm.Model
+	Content string `json:"content"`
+}
+
+func GetAll(c *gin.Context) {
+	var todos []Todo
+	result := db.Find(&todos)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": result.Error.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "all todos",
+		"data":    todos,
+	})
+}
+
+var db *gorm.DB
 
 func main() {
+	loadEnv()
 	r := gin.Default()
+
+	var err error
+
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	failOnError(err, "Error DB Connection")
+
+	db.AutoMigrate(&Todo{})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.GET("/todos", GetAll)
+
+	fmt.Println("Database connection and setup successful")
 	r.Run()
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Printf("Can not load: %v", err)
+	}
+
+	message := os.Getenv("POSTGRES_HOST")
+
+	fmt.Println(message)
 }
